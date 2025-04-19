c4bac1dc2e16b7cd9b98bcada2d491883712cd3e
ItOnltz <hohohubbro@gmail.com>
2025-04-19 23:30:24 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,3 @@
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("SetTeam",  getgenv().Team  or "Marines");
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Iamkhnah/Rubu-Script/refs/heads/main/Main.lua"))()
